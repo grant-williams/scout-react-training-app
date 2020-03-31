376889741bf22ca9edab36256a98c126fec883d4
Grant Williams <grant.williams@credera.com>
2020-03-31 09:54:17 -0500

    Add tests for new app structure

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,45 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { Router } from "react-router-dom";
+import { createMemoryHistory } from "history";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders links on nav bar", () => {
+	const history = createMemoryHistory();
+	const { getByTestId } = render(
+		<Router history={history}>
+			<App />
+		</Router>
+	);
+	const homeLink = getByTestId("home-link");
+	const pokedexLink = getByTestId("pokedex-link");
+
+	expect(homeLink).toBeInTheDocument();
+	expect(pokedexLink).toBeInTheDocument();
+});
+
+test("routes to Pokedex page", () => {
+	const history = createMemoryHistory();
+	history.push("/pokedex");
+	const { getByTestId } = render(
+		<Router history={history}>
+			<App />
+		</Router>
+	);
+	const pokedexHeader = getByTestId("pokedex-header");
+
+	expect(pokedexHeader).toBeInTheDocument();
+});
+
+test("landing on a bad page shows NotFound page", () => {
+	const history = createMemoryHistory();
+	history.push("/some/bad/route");
+	const { getByTestId } = render(
+		<Router history={history}>
+			<App />
+		</Router>
+	);
+	const notFound = getByTestId("not-found");
+
+	expect(notFound).toBeInTheDocument();
 });
