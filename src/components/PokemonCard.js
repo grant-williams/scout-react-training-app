287e79931dcdc22ddb4bc3bb9b80a78f025c32d4
Grant Williams <grant.williams@credera.com>
2020-03-31 15:03:23 -0500

    Add Pokedex page

---
+++ src/components/PokemonCard.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+
+const PokemonCard = ({ pokemon }) => {
+	let history = useHistory();
+
+	const handleClick = () => {
+		history.push(`/pokedex/${pokemon.name}`);
+	};
+
+	return (
+		<div className="pokemon capitalize" onClick={() => handleClick()}>
+			<p>{pokemon.name}</p>
+		</div>
+	);
+};
+
+export default PokemonCard;
--- src/pages/Pokedex.js
+++ src/pages/Pokedex.js
@@ -1,12 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import PokemonCard from "../components/PokemonCard";
 
 const Pokedex = () => {
-    return (
-        <div className="page-header">
-			<h1 className="display-3" data-testid="pokedex-header">Pokedex Page</h1>
-			<p className="lead">subheading</p>
+	const [pokemonList, setPokemonList] = useState([]);
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		fetchPokemonData();
+	}, []);
+
+	const fetchPokemonData = () => {
+		setLoading(true);
+		axios
+			.get("https://pokeapi.co/api/v2/pokemon/?limit=151")
+			.then(response => {
+				setPokemonList(response.data.results);
+			})
+			.catch(error => {
+				throw error;
+			})
+			.finally(() => setLoading(false));
+	};
+
+	return (
+		<div className="page-header">
+			<h1 className="display-3" data-testid="pokedex-header">
+				Pokedex Page
+			</h1>
+			<p className="lead">This page has all Pokemon from the first generation.</p>
+
+			{loading ? (
+				<p>Loading...</p>
+			) : pokemonList.length > 0 ? (
+				<div className="pokemonList">
+					{pokemonList.map((pokemon, index) => (
+						<PokemonCard pokemon={pokemon} key={index} />
+					))}
+				</div>
+			) : (
+				<p>No Pokemon to show.</p>
+			)}
 		</div>
-    );
+	);
 };
 
 export default Pokedex;
